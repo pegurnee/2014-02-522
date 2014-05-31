0d1128e57ab29357af02219addcf4c446eca3ac1
Eddie Gurnee <egurnee@emich.edu>
2014-05-30 22:28:14 -0400

    format

--- Projects/proj0/client.c
+++ Projects/proj0/client.c
@@ -7,14 +7,12 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
-void error(const char *msg)
-{
+void error(const char *msg) {
     perror(msg);
     exit(0);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
@@ -26,8 +24,9 @@ int main(int argc, char *argv[])
     }
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
+    if (sockfd < 0) {
         error("ERROR opening socket");
+    }
     server = gethostbyname(argv[1]);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
@@ -39,18 +38,21 @@ int main(int argc, char *argv[])
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
         error("ERROR connecting");
+    }
     printf("Please enter the message: ");
     bzero(buffer,256);
     fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
+    if (n < 0) {
          error("ERROR writing to socket");
+    }
     bzero(buffer,256);
     n = read(sockfd,buffer,255);
-    if (n < 0) 
+    if (n < 0) {
          error("ERROR reading from socket");
+    }
     printf("%s\n",buffer);
     close(sockfd);
     return 0;
