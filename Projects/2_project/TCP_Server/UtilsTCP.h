a91778936a0778ce847924d84aa8a5850ae6f7d8
Eddie Gurnee <pegurnee@gmail.com>
2014-06-04 17:34:35 -0400

    include UtilsTCP basic

---
+++ Projects/2_project/TCP_Server/UtilsTCP.h
@@ -0,0 +1,28 @@
+/* 
+ * File:   UtilsTCP.h
+ * Author: eddie
+ *
+ * Created on June 4, 2014, 4:35 PM
+ */
+#include <stdbool.h>
+
+#ifndef UTILSTCP_H
+#define	UTILSTCP_H
+
+#define TAG_LOGIN 'L'
+#define TAG_LOGOUT 'O'
+#define TAG_WHO 'W'
+#define TAG_TALK 'T'
+#define DATA_SIZE 20
+
+typedef struct {
+    int type; //the type of message to be sent
+    unsigned int senderID; //the user id that sent the message
+    
+    bool confirm; //if needed, a boolean value to confirm or deny actions
+    char data[DATA_SIZE]; //if needed, an array of chars for extra data
+    
+} Message;
+
+#endif	/* UTILSTCP_H */
+
