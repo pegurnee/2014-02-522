572d07eeaec2dbd879cfdaf9d8cf07b43ed20546
Eddie Gurnee <pegurnee@gmail.com>
2014-06-04 17:49:35 -0400

    include outline comments

--- Projects/2_project/TCP_Server/Server.c
+++ Projects/2_project/TCP_Server/Server.c
@@ -12,7 +12,22 @@
  * 
  */
 int main(int argc, char** argv) {
-
+    //handle command line stuff
+    
+    //create socket
+    //bind to port
+    
+    //two threads, parent: server commands
+    //exit/logout: safely closes down the server
+    //who: displays on the server the current users logged in
+    //say: says a global message from the server
+    
+    //child: sever operation, mainly just retrieving and responding to messages
+    //LOGIN, adds the user to the list of logged in users, sends a message to each other user that the user logged in
+    //LOGOUT, removes the user from the list of logged out users, sends a message to each other user that the user logged out
+    //WHO, returns to the user a list of all of the currently logged in users
+    //TALK, sends a request to start talking with a user
+    
     return (EXIT_SUCCESS);
 }
 
